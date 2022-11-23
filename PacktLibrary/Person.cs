8184ff462e1ea7d544e5c8be6d461742c2692397
Syuzan S <syuzan.sakhoyan@gmail.com>
2022-11-23 20:00:14 +0400

    Bob

---
+++ PacktLibrary/Person.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+namespace Packt.Shared;
+public class Person
+{
+ public string? Name;
+ public WondersOfTheAncientWorld FavoriteAncientWonder ;
+ public WondersOfTheAncientWorld BucketList;
+ public DateTime DateOfBirth;
+ public List<Person> Children = new List<Person>();  
+ public const string Species = "Homo Sapien";
+ public readonly string HomePlanet = "Earth";     
+ public readonly DateTime Instantiated;
+ public Person()
+ {
+    Name = "Unknow";
+    Instantiated=DateTime.Now;
+ }
+ public Person(string initalName, string homePlanet)
+ {
+    Name = initalName;
+    HomePlanet = homePlanet;
+    Instantiated = DateTime.Now;
+ }
+ public (string,int) GetFruit()
+        {
+            return("Appeles",5);
+        }
+}
+
+
+
